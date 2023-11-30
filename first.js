11002001c436c8588edae21afb22528079cc8d38
Anuran12 <anuranarts@gmail.com>
2023-12-01 03:50:18 +0530

    update

--- first.js
+++ first.js
@@ -4,7 +4,7 @@ function sliceSize(dataNum, dataTotal) {
   
   function addSlice(id, sliceSize, pieElement, offset, sliceID, color) {
     $(pieElement).append(
-      "<div class='slice " + sliceID + "'><span></span></div>"
+      "<div class='slice popover-item " + sliceID + "' data-tooltip-text='this shape is blue'><span></span></div>"
     );
     var offset = offset - 1;
     var sizeRotation = -179 + sliceSize;
@@ -106,4 +106,87 @@ function sliceSize(dataNum, dataTotal) {
   }
   
   createPieCharts();
+
+
+
+
+
+
+  /* Script for popover positioning */ 
+(function(selector, horizontalOffset, verticalOffset) {
+  var items;
+  
+  selector = selector || '.popover-item';
+  horizontalOffset = horizontalOffset || 5;
+  verticalOffset = verticalOffset || 5;
+  
+  items = document.querySelectorAll(selector);
+  items = Array.prototype.slice.call(items);
+  
+  items.forEach(function(item) {
+    // Every time the pointer moves over the element the 
+    //  CSS-rule in overwritten with new values for 
+    //  top and left.
+    item.addEventListener('mousemove', function(e) {
+      let countCssRules = document.styleSheets[0].cssRules.length;
+      var rightSpace = document.body.clientWidth - e.pageX;
+      if( rightSpace > 300 ){
+        var newRule = selector +
+        ':hover .popover-content {  ' + 
+                       'right:auto; left: ' + (horizontalOffset + e.offsetX) + 'px; ' +
+                       'top: ' +  (e.offsetY + verticalOffset) + 'px; }';
+      }else{
+         var newRule = selector +
+        ':hover .popover-content {  ' + 
+                       'left:auto; right: ' + (item.offsetWidth - e.offsetX) + 'px; ' +
+                       'top: ' +  (e.offsetY + verticalOffset) + 'px; }';
+      }
+     
+      
+      document.styleSheets[0].insertRule(newRule, countCssRules);
+    });
+  });
+})('.popover-item', 10, 5);
+
+
+
+
+
+
+
+
+
+
+
+
+// Collect all elements with "shape" class
+var shapesCollection = document.getElementsByClassName("popover-item");
+
+// Get the tooltip container
+var tooltip = document.getElementById('tooltip');
+
+// Add events for tooltip creation
+for (var i = 0; i < shapesCollection.length; i++) {
+   shapesCollection[i].addEventListener('mousemove', showTooltip);
+   shapesCollection[i].addEventListener('mouseout', hideTooltip);
+}
+
+function showTooltip(evt) {
+   
+   // Set tooltip to visible
+   tooltip.style.visibility = "visible"
+   
+   // Updates position of the tooltip when the pointer is moving
+   // (Mouse coordinates relative to client area + arbitrary offset)
+   tooltip.style.top = (evt.clientY + 20) + "px";
+   tooltip.style.left = (evt.clientX + 15) + "px";
+   
+   // Change text inside the tooltip getting text from the relative attribute on the hovered div 
+   tooltip.innerHTML = evt.target.getAttributeNS(null, "data-tooltip-text");
+}
+
+// Shut down tooltip when mouse pointer leaves the shape 
+function hideTooltip() {
+   tooltip.style.visibility = "hidden"
+}
   
